df71694f35fae7461fa4ba9e4334b978ada84431
Michael <michael@designquest.com.hk>
2020-10-06 17:43:03 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+https://docs.mapbox.com/help/how-mapbox-works/mapbox-data/
+
+## Layers
+https://docs.mapbox.com/mapbox-gl-js/style-spec/layers/
+
+## Events
+https://docs.mapbox.com/mapbox-gl-js/api/map/#map-events
---
+++ flight-viewer.js
@@ -0,0 +1,44 @@
+mapboxgl.accessToken = "pk.eyJ1IjoiZHFtaWNoYWVsIiwiYSI6ImhMSDVjVUkifQ.6StMLfY4IIePFbKmZjfQJw";
+var map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/dqmichael/ckfxlwp4h0d5h1arznvqihbw1", // stylesheet location
+    center: [114.304060, 22.309058], // starting position [lng, lat]
+    zoom: 16 // starting zoom
+});
+
+map.on("load", function () {
+    map.loadImage(
+        "./images/drone.png",
+        function (error, image) {
+            if (error) throw error;
+            map.addImage("drone-img", image);
+            map.addSource("drone-src", {
+                "type": "geojson",
+                "data": {
+                    "type": "FeatureCollection",
+                    "features": [{
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [114.304060, 22.309058]
+                        }
+                    }]
+                }
+            });
+            map.addLayer({
+                "id": "drones",
+                "type": "symbol",
+                "source": "drone-src",
+                "layout": {
+                    "icon-image": "drone-img",
+                    "icon-size": 0.25,
+                    "icon-rotate": 270,
+                    "icon-allow-overlap": true,
+                }
+            });
+            console.log(map.getLayer("drones"));
+            map.setLayoutProperty("drones", "icon-rotate", 45)
+        }
+    );
+
+});
